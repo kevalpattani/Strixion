32062003193f5b0e61cc7a305ebda077ffa7922c
Keval Pattani <87653668+kevalpattani@users.noreply.github.com>
2026-08-30 23:41:01 +0530

    fix: include ALUop handling

--- Src/ALU/ALU.v
+++ Src/ALU/ALU.v
@@ -25,6 +25,7 @@ ALU Control,         Operation,   Corresponding RISC-V Instructions
 */
 
 module ALU(
+    input ALUop,
     input [31:0] Src_A, Src_B,
     input [4:0] alucontrol,
     input branch_token,
@@ -44,112 +45,119 @@ divider_ALU div(.a(Src_A),.b(Src_B),.func3(alucontrol[1:0]),.result_sum(result_d
 
 always @(*)
 begin
-    result_alu = 32'b0;
-    zero_pin = 1'b0;
-    
-    casex(alucontrol)
-        5'b00000: begin
-            result_alu = Src_A & Src_B; 
-        end
-        5'b00001: begin
-            result_alu = Src_A | Src_B;
-        end
-        5'b00010: begin
-            result_alu = result_add;
-        end
-        5'b00011: begin
-            result_alu = Src_A ^ Src_B;
-        end
-        5'b00100: begin
-            result_alu = Src_A << Src_B[4:0];
-        end
-        5'b00101: begin
-            result_alu = Src_A >> Src_B[4:0];
-        end
-        5'b00110: begin
-            result_alu = result_sub;
-            if (branch_token) begin // branch_token 1 -> BNE
-                if (result_alu == 32'b0) begin
-                    zero_pin = 1'b0;
+    if (!ALUop) begin
+        result_alu = 32'b0;
+        zero_pin = 1'b0;
+        
+        casex(alucontrol)
+            5'b00000: begin
+                result_alu = Src_A & Src_B; 
+            end
+            5'b00001: begin
+                result_alu = Src_A | Src_B;
+            end
+            5'b00010: begin
+                result_alu = result_add;
+            end
+            5'b00011: begin
+                result_alu = Src_A ^ Src_B;
+            end
+            5'b00100: begin
+                result_alu = Src_A << Src_B[4:0];
+            end
+            5'b00101: begin
+                result_alu = Src_A >> Src_B[4:0];
+            end
+            5'b00110: begin
+                result_alu = result_sub;
+                if (branch_token) begin // branch_token 1 -> BNE
+                    if (result_alu == 32'b0) begin
+                        zero_pin = 1'b0;
+                    end
+                    else begin
+                        zero_pin = 1'b1;
+                    end
+                end
+                else begin// branch_token 0 -> BEQ
+                    if (result_alu == 32'b0) begin
+                        zero_pin = 1'b1;
+                    end
+                    else begin
+                        zero_pin = 1'b0;
+                    end
+                end    
+            end
+            5'b00111: begin
+                result_alu = $signed(Src_A) >>> Src_B[4:0]; // $signed is added cause otherwise the operation will not be the arithematic shift as intended, it will be logical shift 
+            end
+            5'b01000: begin
+                if ($signed(Src_A) < $signed(Src_B)) begin // blt and bge -> {rs1 < rs2} and {rs1 > rs2} signed
+                    result_alu = 32'b1;
+                    if (branch_token) begin // bge -> {rs1 > rs2}
+                        zero_pin = 1'b0;
+                    end
+                    else begin // blt -> {rs1 < rs2}
+                        zero_pin = 1'b1;
+                    end
                 end
                 else begin
-                    zero_pin = 1'b1;
+                    result_alu = 32'b0;
+                    if (branch_token) begin // bge -> {rs1 > rs2}
+                        zero_pin = 1'b1;
+                    end
+                    else begin // blt -> {rs1 < rs2}
+                        zero_pin = 1'b0;
+                    end
                 end
             end
-            else begin// branch_token 0 -> BEQ
-                if (result_alu == 32'b0) begin
-                    zero_pin = 1'b1;
+            5'b01001: begin
+                if (Src_A < Src_B) begin
+                    result_alu = 32'b1;
+                    if (branch_token) begin // bgeu -> {rs1 > rs2}
+                        zero_pin = 1'b0;
+                    end
+                    else begin // bltu -> {rs1 < rs2}
+                        zero_pin = 1'b1;
+                    end
                 end
                 else begin
-                    zero_pin = 1'b0;
-                end
-            end    
-        end
-        5'b00111: begin
-            result_alu = $signed(Src_A) >>> Src_B[4:0]; // $signed is added cause otherwise the operation will not be the arithematic shift as intended, it will be logical shift 
-        end
-        5'b01000: begin
-            if ($signed(Src_A) < $signed(Src_B)) begin // blt and bge -> {rs1 < rs2} and {rs1 > rs2} signed
-                result_alu = 32'b1;
-                if (branch_token) begin // bge -> {rs1 > rs2}
-                    zero_pin = 1'b0;
-                end
-                else begin // blt -> {rs1 < rs2}
-                    zero_pin = 1'b1;
+                    result_alu = 32'b0;
+                    if (branch_token) begin // bgeu -> {rs1 > rs2}
+                        zero_pin = 1'b1;
+                    end
+                    else begin // bltu -> {rs1 < rs2}
+                        zero_pin = 1'b0;
+                    end
                 end
             end
-            else begin
+            
+            /*
+                      Src_A     Src_B
+                MUL - signed x signed - Lower 32                                   
+                MULH - signed x signed - Upper 32
+                MULHSU - signed x unsigned - Upper 32      
+                MULHU - unsigned x unsigned - Upper 32 
+            */
+            
+            5'b100xx: begin
+                result_alu = result_mul;
+            end
+            
+            5'b101xx: begin // division all
+                result_alu = result_div;
+            end
+            
+            default: begin
                 result_alu = 32'b0;
-                if (branch_token) begin // bge -> {rs1 > rs2}
-                    zero_pin = 1'b1;
-                end
-                else begin // blt -> {rs1 < rs2}
-                    zero_pin = 1'b0;
-                end
             end
-        end
-        5'b01001: begin
-            if (Src_A < Src_B) begin
-                result_alu = 32'b1;
-                if (branch_token) begin // bgeu -> {rs1 > rs2}
-                    zero_pin = 1'b0;
-                end
-                else begin // bltu -> {rs1 < rs2}
-                    zero_pin = 1'b1;
-                end
-            end
-            else begin
-                result_alu = 32'b0;
-                if (branch_token) begin // bgeu -> {rs1 > rs2}
-                    zero_pin = 1'b1;
-                end
-                else begin // bltu -> {rs1 < rs2}
-                    zero_pin = 1'b0;
-                end
-            end
-        end
-        
-        /*
-                  Src_A     Src_B
-            MUL - signed x signed - Lower 32                                   
-            MULH - signed x signed - Upper 32
-            MULHSU - signed x unsigned - Upper 32      
-            MULHU - unsigned x unsigned - Upper 32 
-        */
-        
-        5'b100xx: begin
-            result_alu = result_mul;
-        end
-        
-        5'b101xx: begin // division all
-            result_alu = result_div;
-        end
-        
-        default: begin
-            result_alu = 32'b0;
-        end
-                
-    endcase
+                    
+        endcase
+    end
+    else
+    begin
+        result_alu = 32'b0;
+        zero_pin = 1'b0;
+    end
 end
 
 endmodule
